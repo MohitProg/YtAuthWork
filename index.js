7bfd52e0ebd90b1a6474930f50454f9a5263a011
MohitProg <mohitjj999@gmail.com>
2025-03-27 10:25:18 +0530

    how to protect api routes

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 import "dotenv/config.js";
-
 import express from "express";
 import { db } from "./db/db.js";
 const app = express();
-import userRoute from "./routes/userRoutes.js"
+import userRoute from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
+import { AuthApi } from "./middlware/AuthApi.js";
 
 const PORT = process.env.PORT || 3000;
 
+//
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/user", userRoute);
 
 
-// 
-app.use(express.json())
-app.use(cookieParser())
-
-
-
-
-app.use("/api/user",userRoute)
+app.get("/home",AuthApi,(req,res)=>{
+  console.log(req.userid)
+  console.log("route is running")
+  return res.send("This is my home page")
+})
 
 db().then(() => {
   app.listen(PORT, () => {
